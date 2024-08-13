482568e88599e9b339f89e449b5ff789ffe5ae8e
Bildad58 <162963842+Bildad58@users.noreply.github.com>
2024-08-13 23:46:04 +0300

    Delete django-models/LibraryProject directory

--- django-models/LibraryProject/relationship_app/query_samples.py
+++
@@ -1,8 +0,0 @@
-from .models import Author, Library, Librarian 
-
-# query for all bookks by specific author
-book = Author.object.get( author='')
-
-Library = Library.objects.all()
-
-librarian = Librarian.objects.get
